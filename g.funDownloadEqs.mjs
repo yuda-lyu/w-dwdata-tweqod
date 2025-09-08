cc001e455bf9f2d3e2229f69c20251204dd55cff
yuda-lyu <firsemisphere@gmail.com>
2025-09-08 15:09:28 +0800

    modify: update pks

--- g.funDownloadEqs.mjs
+++ g.funDownloadEqs.mjs
@@ -88,7 +88,7 @@ ev.on('change', (msg) => {
 // change { event: 'proc-callfun-getCurrent', msg: 'start...' }
 // change { event: 'proc-callfun-getCurrent', msg: 'done' }
 // change { event: 'compare', msg: 'start...' }
-// change { event: 'compare', msg: 'done' }
+// change { event: 'compare', numRemove: 0, numAdd: 2, numModify: 0, numSame: 0, msg: 'done' }
 // change { event: 'proc-add-callfun-add', id: '114101', msg: 'start...' }
 // change { event: 'proc-add-callfun-add', id: '114101', msg: 'done' }
 // change { event: 'proc-add-callfun-add', id: '114102', msg: 'start...' }
